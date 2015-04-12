58df1d6dd2500e1c6374ce8bb2fd71e7861ab42d
Elena Torro <elenatorro@gmail.com>
2015-04-12 19:36:45 +0200

    changed the way we parse the response from the database

--- app/assets/scripts/controllers/synonymsController.js
+++ app/assets/scripts/controllers/synonymsController.js
@@ -3,27 +3,16 @@ angular.module('PlaylinguaApp')
   '$scope', "$http", "$routeParams","User", "Level",
   function($scope, $http, $routeParams, User, Level) {
     Level.get({'levelnumber': $routeParams.level}).$promise.then(function(level) {
-      delete level.excercises._id;
+      $scope.contentArray = _.sample(level.elements, 3);
       $scope.level = level;
-      $scope.contentArray = level.excercises.content;
-      console.log($scope.content);
-      $scope.words = level.excercises.content[0].elements.words.replace(/[,;:.]/g, "").split(" ");
-      $scope.dragWords = level.excercises.content[0].elements.selected;
+      console.log(level);
     });
 
     User.get().$promise.then(function(user) {
       $scope.user = user;
-      $scope.game = user.game;
     });
 
-    $scope.onDrop = function(word, object) {
-      if (word == object.word) {
-        console.log("yay");
-      } else {
-        $scope.level.lifes.pop();
-      }
+    $scope.getTimes = function(number) {
+      return new Array(number);
     };
-
-    $scope.sliderFx = new SliderFx( document.getElementById('slideshow'), {
-          easing : 'cubic-bezier(.8,0,.2,1)'});
 }]);
