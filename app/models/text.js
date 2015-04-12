ae41739ee2645888cce188540cdb849ce59bdf22
Elena Torro <elenatorro@gmail.com>
2015-04-12 19:44:11 +0200

    default values changed

--- app/models/text.js
+++ app/models/text.js
@@ -3,14 +3,12 @@ var mongoose = require('mongoose');
 var textSchema = mongoose.Schema({
     text : {
       name  : String,
-      content : [{
-        level : Number,
-        elements : [{
-          words    : String,
-          selected : [{
-                word  : String,
-                value : String
-          }]
+      level : Number,
+      elements : [{
+        words    : String,
+        selected : [{
+              word  : String,
+              value : String
         }]
       }]
     }
