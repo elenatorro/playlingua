6ce4ccaecd66eb69fd0596b02e6b2d2e6b820b9a
Elena Torro <elenatorro@gmail.com>
2015-04-16 16:51:03 +0200

    pushing database to heroku

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
+// module.exports = {
+// 	'url' : 'mongodb://localhost/knoldus'
+// };
+
 module.exports = {
-	'url' : 'mongodb://localhost/knoldus' 
+	'url' : 'mongodb://cryptic-cove-9005.herokuapp.com/knoldus'
 };
