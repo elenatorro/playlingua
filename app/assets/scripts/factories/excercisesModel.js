e377cf338b886ff43b10cf8b7b972fe13167a008
Elena Torro <elenatorro@gmail.com>
2015-05-04 17:34:58 +0200

    added total points to excercise

--- app/assets/scripts/factories/excercisesModel.js
+++ app/assets/scripts/factories/excercisesModel.js
@@ -4,6 +4,11 @@ angular.module('PlaylinguaApp').factory('Excercises',
     function Excercises(data) {
         angular.extend(this, data);
         var self = this;
+        console.log(self);
+        self.excercises.forEach(function(excercise) {
+           excercise.totalScore = excercise.levels[0].totalScore + excercise.levels[1].totalScore + excercise.levels[2].totalScore;
+        })
+       
     };
 
     var resourceExcercises = $resource(
