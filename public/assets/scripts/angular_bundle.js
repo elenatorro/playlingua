0d178e040e96d798223684afd762d8a38ba42872
Elena Torro <elenatorro@gmail.com>
2015-05-05 09:35:51 +0200

    style changed

--- public/assets/scripts/angular_bundle.js
+++ public/assets/scripts/angular_bundle.js
@@ -306,13 +306,12 @@ angular.module('PlaylinguaApp')
 angular.module('PlaylinguaApp').factory('Excercises', 
   ['$resource', '$http', '$q', function($resource, $http, $q) {
     function Excercises(data) {
-        angular.extend(this, data);
-        var self = this;
-        console.log(self);
-        self.excercises.forEach(function(excercise) {
-           excercise.totalScore = excercise.levels[0].totalScore + excercise.levels[1].totalScore + excercise.levels[2].totalScore;
-        })
-       
+      angular.extend(this, data);
+      var self = this;
+      console.log(self);
+      self.excercises.forEach(function(excercise) {
+         excercise.totalScore = excercise.levels[0].totalScore + excercise.levels[1].totalScore + excercise.levels[2].totalScore;
+      })
     };
 
     var resourceExcercises = $resource(
