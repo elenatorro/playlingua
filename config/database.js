164b8bca367d726c7e6ce34762a5e43accdd4bf5
Elena Torro <elenatorro@gmail.com>
2015-10-13 21:16:59 +0200

    database data

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
-module.exports = {
-	'url' : 'mongodb://localhost/knoldus'
-};
-
 // module.exports = {
-// 	'url' : 'mongodb://cryptic-cove-9005.herokuapp.com/knoldus'
+// 	'url' : 'mongodb://localhost/knoldus'
 // };
+
+module.exports = {
+	'url' : 'mongodb://heroku_app35873561:bbr70k5q4kgou788iu8mqpsmeg@ds061651.mongolab.com:61651/heroku_app35873561?replicaSet=rs-ds061651'
+};
