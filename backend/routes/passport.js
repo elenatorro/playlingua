13f6b78093d7ff724fd8e6141ca1163ae4075d61
Elena Torro <elenatorro@gmail.com>
2016-07-04 18:55:49 +0200

    trivial fix

--- backend/routes/passport.js
+++ backend/routes/passport.js
@@ -6,29 +6,30 @@ var
     TwitterStrategy =     require('passport-twitter').Strategy,
     GoogleStrategy =      require('passport-google-oauth').OAuth2Strategy,
 
-    facebookStrategySecrets = {
-        clientID:     config.passport.facebook.FACEBOOK_APP_ID,
-        clientSecret: config.passport.facebook.FACEBOOK_APP_SECRET,
-        callbackURL:  config.passport.facebook.FACEBOOK_CALLBACK_URL
-    }
-
-    googleStragegySecrets = {
-        clientID:     config.passport.google.GOOGLE_CONSUMER_KEY,
-        clientSecret: config.passport.google.GOOGLE_CONSUMER_SECRET,
-        callbackURL:  config.passport.google.GOOGLE_CALLBACK_URL
-    }
-
-    twitterStrategySecrets = {
-        consumerKey:    config.passport.twitter.TWITTER_CONSUMER_KEY,
-        consumerSecret: config.passport.twitter.TWITTER_CONSUMER_SECRET,
-        callbackURL:    config.passport.twitter.TWITTER_CALLBACK_URL
-    }
-
     User =     require('../../app/models/user'),
     Exercise = require('../../app/models/exercise');
 
 module.exports = function(passport) {
  var serializeUser, deserializeUser, loginStrategy, signupStrategy, localapikey, facebookStrategy, googleStrategy, twitterStrategy;
+
+ var facebookStrategySecrets = {
+     clientID:     config.passport.facebook.FACEBOOK_APP_ID,
+     clientSecret: config.passport.facebook.FACEBOOK_APP_SECRET,
+     callbackURL:  config.passport.facebook.FACEBOOK_CALLBACK_URL
+ }
+
+ var googleStrategySecrets = {
+     clientID:     config.passport.google.GOOGLE_CONSUMER_KEY,
+     clientSecret: config.passport.google.GOOGLE_CONSUMER_SECRET,
+     callbackURL:  config.passport.google.GOOGLE_CALLBACK_URL
+ }
+
+ var twitterStrategySecrets = {
+     consumerKey:    config.passport.twitter.TWITTER_CONSUMER_KEY,
+     consumerSecret: config.passport.twitter.TWITTER_CONSUMER_SECRET,
+     callbackURL:    config.passport.twitter.TWITTER_CALLBACK_URL
+ }
+
 
  loginStrategy =    new LocalStrategy(config.passport.localStrategy, User.schema.methods.login)
  signupStrategy =   new LocalStrategy(config.passport.localStrategy, User.schema.methods.signup)
